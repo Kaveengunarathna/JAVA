48b5283ee8145df6caa763a215d8c1969c989744
kaveen20030408 <it23746664@my.sliit.lk>
2025-03-21 01:14:30 +0530

    Added Java basic

---
+++ Javabasic.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+// HelloWorld.java
+public class Javabasic {
+    public static void main(String[] args) {
+        String name = "My name is kaveen";
+        System.out.println(name);
+    }
+}
---
+++ number.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+
+// HelloWorld.java
+public class number {
+    public static void main(String[] args) {
+        int x = 10;
+        x += 5;
+        System.out.println(x);
+    }
+}
