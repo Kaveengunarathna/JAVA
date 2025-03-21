786bec854bf723faff0482bc486ec1b2db2c6cc7
kaveen20030408 <it23746664@my.sliit.lk>
2025-03-21 08:57:32 +0530

    Added new java file

---
+++ recrution.java
@@ -0,0 +1,14 @@
+public class recrution {
+    public static void main(String[] args) {
+        int result = sum(10);
+        System.out.println(result);
+
+    }
+    public static int sum(int n) {
+        if(n>0){
+            return n+sum(n-1);
+        }else{
+            return 0;
+        }
+    }
+}
